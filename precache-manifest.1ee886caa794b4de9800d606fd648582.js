44f75f6307f66bf0f06c3e35c2848f52bf3af0f7
Johan Champlon <johanchamplon@sfr.fr>
2019-08-14 15:10:29 +0200

    Updates

---
+++ precache-manifest.1ee886caa794b4de9800d606fd648582.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358c127a7c6b2d198d1234f4608a45db",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "12ddb4973569f9acde42",
+    "url": "/react-monsters/static/css/main.80606718.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/react-monsters/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "12ddb4973569f9acde42",
+    "url": "/react-monsters/static/js/main.330925a8.chunk.js"
+  },
+  {
+    "revision": "ca96d79ea22a083e77dd",
+    "url": "/react-monsters/static/js/runtime~main.9e6231db.js"
+  }
+]);
