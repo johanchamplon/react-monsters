d549ca4381b6d111ed11a4bb5bc695da4e611ad9
Johan Champlon <johanchamplon@sfr.fr>
2019-08-14 15:00:49 +0200

    Design app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { CardList } from './component/cardList/CardList.component.jsx'
+import { SearchBox } from './component/search-box/SearchBox.component.jsx'
+
 import './App.css';
 
 class App extends Component {
@@ -25,15 +27,17 @@ class App extends Component {
 
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
-        <input
-          type="search"
-          placeholder="search monsters"
-          onChange={this.handleChange}
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeHolder="Search Monster"
+          handleChange={this.handleChange}
         />
         <CardList
-          monsters={this.state.monsters}
+          monsters={filteredMonsters}
         />
       </div>
     );
